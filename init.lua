865220c5281ab41e13d5108ee9f64f150569e5ce
zachary <groff.zackm@gmail.com>
2022-12-22 15:13:22 -0500

    "extract statusline.lua"

--- init.lua
+++ init.lua
@@ -51,28 +51,6 @@ vim.cmd("let g:VM_maps = {}")
 vim.cmd("let g:VM_maps['Add Cursor Down'] = '<A-Down>'")
 vim.cmd("let g:VM_maps['Add Cursor Up'] = '<A-Up>'")
 
--- custom statusline
-function get_cwd()
-	local pwd = vim.fn.system("pwd 2> /dev/null | tr -d '\n'")
-	return '/' .. string.match(pwd, "[^/]+$")
-end
-
-function get_git_branch()
-	local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
-	if branch ~= "" then
-		return branch
-	else
-		return "None"
-	end
-end
-
-function setup_statusline()
-	vim.cmd('let branch="[' .. get_cwd() ..  ':' .. get_git_branch() .. ']"')
-	vim.cmd('set statusline=%f%=%-25.75{branch}')
-	vim.cmd('hi statusline guibg=teal guifg=grey78')
-end
-setup_statusline()
-
 -- lsp
 require'lspconfig'.pyright.setup{}
 
@@ -80,3 +58,4 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 lsp.setup()
 
+require('statusline').setup()
