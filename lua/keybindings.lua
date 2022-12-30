7f9b0d8ca2a5d503287a71f7bb63e97c67656bbd
zachary <groff.zackm@gmail.com>
2022-12-30 10:44:52 -0500

    "add keybinding for :TroubleToggle"

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,10 +1,15 @@
 function setup()
+	-- vimspector
 	vim.cmd("let g:vimspector_enable_mappings = 'HUMAN'")
 	vim.cmd("nmap <F2> :VimspectorReset<CR>")
 
+	-- visual-multi
 	vim.cmd("let g:VM_maps = {}")
 	vim.cmd("let g:VM_maps['Add Cursor Down'] = '<A-Down>'")
 	vim.cmd("let g:VM_maps['Add Cursor Up'] = '<A-Up>'")
+
+	-- trouble
+	vim.cmd("nmap <C-t> :TroubleToggle<CR>")
 end
 
 M = {
