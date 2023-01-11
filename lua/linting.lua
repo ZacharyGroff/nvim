b01fd9d9366a0f21f6c2d39ae5671e9b08e43142
zachary <groff.zackm@gmail.com>
2023-01-11 12:20:43 -0500

    "use black, ssort, and isort on python file save instead of flake8"

--- lua/linting.lua
+++ lua/linting.lua
@@ -3,11 +3,7 @@ vim.api.nvim_create_autocmd(
 	"BufWritePost",
 	{
 		pattern = "*.py",
-		callback = function()
-			local current_file = vim.fn.expand('%')
-			os.execute("autopep8 -i " .. current_file)
-			vim.cmd('checktime')
-		end,
+		command = "!ssort % && isort % && black %"
 	}
 )
 
