e7c7c4255ec9a7b2b153a56e0c355bac65dfff52
zachary <groff.zackm@gmail.com>
2023-01-24 07:38:49 -0500

    add more plugins and keybindings

--- init.lua
+++ init.lua
@@ -13,6 +13,13 @@ Plug 'puremourning/vimspector'
 -- lukas-reineke/indent-blankline.nvim
 Plug 'lukas-reineke/indent-blankline.nvim'
 
+-- tpope/vim-fugitive
+Plug 'tpope/vim-fugitive'
+
+-- nvim-telescope/telescope
+Plug 'nvim-lua/plenary.nvim'
+Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.1' })
+
 -- nvim-treesitter/nvim-treesitter
 Plug 'nvim-treesitter/nvim-treesitter'
 
@@ -51,6 +58,20 @@ require'lspconfig'.pyright.setup{}
 
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
+lsp.on_attach(function(client, bufnr)
+	local opts = {buffer = bufnr, remap = false}
+
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
 lsp.setup()
 
 -- treesitter
@@ -65,29 +86,12 @@ require'nvim-treesitter.configs'.setup {
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
-
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = { "c", "rust" },
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,4 +1,5 @@
 function setup()
+
 	-- vimspector
 	vim.cmd("let g:vimspector_enable_mappings = 'HUMAN'")
 	vim.cmd("nmap <F2> :VimspectorReset<CR>")
@@ -10,6 +11,16 @@ function setup()
 
 	-- trouble
 	vim.cmd("nmap <C-t> :TroubleToggle<CR>")
+
+	-- fugitive
+	vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+
+	-- telescope (requires brew install ripgrep)
+	local builtin = require('telescope.builtin')
+	vim.keymap.set('n', '<C-f>', builtin.git_files, {})
+	vim.keymap.set('n', '<leader>f', function()
+		builtin.grep_string({ search = vim.fn.input("Grep > ") })
+	end)
 end
 
 M = {
