3a8ffe5f8564b13ccfcaf5347e2a8c5c2e7272b3
Zachary Groff <groff.zackm@gmail.com>
2024-02-10 20:46:21 -0500

    Adds rust support

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,11 +1,16 @@
 return {
   "nvim-neotest/neotest",
+  dependencies = {
+    "rouge8/neotest-rust",
+  },
   opts = {
     -- Can be a list of adapters like what neotest expects,
     -- or a list of adapter names,
     -- or a table of adapter names, mapped to adapter configs.
     -- The adapter will then be automatically loaded with the config.
-    adapters = {},
+    adapters = {
+      ["neotest-rust"] = {},
+    },
     -- Example for loading neotest-go with a custom config
     -- adapters = {
     --   ["neotest-go"] = {
